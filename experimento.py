cd79712693beca93ebd13deb86443f58247fd092
Guilherme Vieira <guilherme.camara@edu.pucrs.br>
2025-05-03 20:22:55 -0300

    print ffmplay log in h2

--- experimento.py
+++ experimento.py
@@ -35,11 +35,8 @@ def run():
 	# HTB: hierarchical token bucket
 	# da prioridade para o tráfego de vídeo
     s1.cmd('tc qdisc add dev s1-eth3 root handle 1: htb default 30')
-	# Classe de alta prioridade para vídeo, garantindo 6Mbit
     s1.cmd('tc class add dev s1-eth3 parent 1: classid 1:10 htb rate 6Mbit ceil 10Mbit prio 0')
-	# Classe de baixa prioridade para o resto (iperf), com até 4Mbit
     s1.cmd('tc class add dev s1-eth3 parent 1: classid 1:20 htb rate 2Mbit ceil 4Mbit prio 1')
-	# Filtro: identifica tráfego de vídeo pela porta 5004 (troque se for diferente)
     s1.cmd('tc filter add dev s1-eth3 protocol ip parent 1:0 prio 1 u32 match ip dport 5004 0xffff flowid 1:10')
 
 	# TBF: Token Bucket Filter
@@ -78,7 +75,6 @@ def run():
     s1.cmd('tc filter add dev s1-eth3 protocol ip parent 1:0 prio 2 u32 match ip dport 5001 0xffff flowid 1:20')
 
     print("Iniciando transmissão RTP de h1 para h2...")
-
     h1.cmd(
         'ffmpeg -re -i video.mp4 '
         '-map 0:v:0 -c:v libx264 -preset ultrafast -tune zerolatency '
@@ -88,21 +84,16 @@ def run():
         '-f rtp rtp://10.0.0.2:5006?pkt_size=1200 '
         '-sdp_file video.sdp > /tmp/ffmpeg.log 2>&1 &'
     )
-
     sleep(2)
 
     print("Iniciando ffplay em h2...")
-
     h2.cmd('ffplay -protocol_whitelist "file,udp,rtp" -fflags nobuffer -flags low_delay -i video.sdp '
        '> /tmp/ffplay.log 2>&1 &')
-
     sleep(2)
-    
     print("Iniciando monitoramento da interface do link s1 <-> s2...")
     monitor = s1.popen('ifstat -i s1-eth3 0.5', stdout=sys.stdout)
 
     sleep(10)
-
     num_streams = 3
     duration = 20
     print(f"Iniciando {num_streams} fluxo(s) iperf UDP de h3 para h4 por {duration} segundos...")
@@ -114,6 +105,9 @@ def run():
 
     print("Encerrando monitoramento...")
     monitor.terminate()
+
+    print("Conteúdo do ffplay.log (h2):")
+    print(h2.cmd("cat /tmp/ffplay.log"))
 
     print("Encerrando rede...")
     net.stop()
